1f42874dc115554c757dac386d07060dda6a4892
Rocky3112 <alomgirhossainrocky@gmail.com>
2023-11-16 00:47:39 +0600

    login page design completed

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP</title>
+    <link rel="stylesheet" href="./styles.css">
+
+</head>
+
+<body>
+    <div class="formContainer">
+        <div class="formControl">
+        <form>
+            <input class="inputAll" type="email" name="email" placeholder="Email Address">
+            <br>
+            <input class="inputAll" type="password" name="password" placeholder="Password">
+            <br>
+           
+            <div class="btn">
+                <button type="submit">Send</button>
+            </div>
+        </form>
+        </div>
+    </div>
+</body>
+
+</html>
+<?php
+
+?>
